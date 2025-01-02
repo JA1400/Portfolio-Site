8a146884e3790c644a600ef24a3428a89594a583
Angel Federico <jose.angel9735@gmail.com>
2025-01-02 01:51:43 -0600

    Initial App Created

---
+++ portfolioSite/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss',
+})
+export class NavBarComponent {
+  menuToggle: boolean = false;
+
+  toggleMenu(): void {
+    this.menuToggle = !this.menuToggle;
+    console.log('Click');
+  }
+}
