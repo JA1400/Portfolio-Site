e6ed34b6c668adb22b0524c31b87ea602ae65be2
Angel Federico <jose.angel9735@gmail.com>
2025-02-17 14:03:59 -0600

    Deploy Fixes

--- portfolioSite/src/environments/environment.prod.ts
+++ portfolioSite/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  PUBLIC_KEY: process.env['EJS_PUBLIC_KEY'] || '',
-  TEMPLATE_ID: process.env['EJS_TEMPLATE_ID'] || '',
-  SERVICE_ID: process.env['EJS_SERVICE_ID'] || '',
+  PUBLIC_KEY: '',
+  TEMPLATE_ID: '',
+  SERVICE_ID: '',
 };
