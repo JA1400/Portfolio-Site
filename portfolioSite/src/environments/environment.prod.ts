cb6a2f8f7788e8245a348291cb7c002d8ebcc1e9
Angel Federico <jose.angel9735@gmail.com>
2025-02-17 04:29:15 -0600

    Fixed Deploy Errors

--- portfolioSite/src/environments/environment.prod.ts
+++ portfolioSite/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  PUBLIC_KEY: process.env['PUBLIC_KEY'] || '',
-  TEMPLATE_ID: process.env['TEMPLATE_ID'] || '',
-  SERVICE_ID: process.env['SERVICE_ID'] || '',
+  PUBLIC_KEY: '@@PUBLIC_KEY@@', // Placeholder
+  TEMPLATE_ID: '@@TEMPLATE_ID@@',
+  SERVICE_ID: '@@SERVICE_ID@@',
 };
